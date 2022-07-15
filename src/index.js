72166baf8cee31e0f692420c5a75214c5ed8d107
michael.ly2839 <michael.ly2839@gmail.com>
2022-07-15 16:19:31 -0400

    added the plane trails and rendering the background rings

--- src/index.js
+++ src/index.js
@@ -14,9 +14,16 @@ import anime from 'https://cdn.skypack.dev/animejs@3.2.1';
 //adding scene
 const scene = new Scene();
 
+//adding a scene for rings
+const ringScene = new Scene();
+
 //adding camera
 const camera = new PerspectiveCamera(45, innerWidth/innerHeight, 0.1, 1000);
 camera.position.set(0, 15, 50);
+
+//adding ring camera
+const ringCamera = new PerspectiveCamera(45, innerWidth/innerHeight, 0.1, 1000);
+ringCamera.position.set(0, 0, 50);
 
 //adding renderer
 const renderer = new WebGLRenderer({ antialias: true, alpha: true});
@@ -55,6 +62,44 @@ scene.add(sunLight);
         .setDataType(FloatType)
         .loadAsync("assets/drackenstein_quarry_4k.hdr");
     let envMap = pmrem.fromEquirectangular(envmapTexture).texture;
+
+    //adding rings
+    const ring1 = new Mesh(
+        new RingGeometry(15, 13.5, 80, 1, 0),
+        new MeshPhysicalMaterial({
+            color: new Color("#FFCB8E").convertSRGBToLinear().multiplyScalar(200),
+            roughness: 0.25,
+            envMap,
+            envmapIntensity: 1.8,
+            side: DoubleSide,
+            transparent: true,
+            opacity: 0.35,
+        })
+    )
+    ringScene.add(ring1);
+
+    const ring2 = new Mesh(
+        new RingGeometry(16.5, 15.75, 80, 1, 0),
+        new MeshPhysicalMaterial({
+            color: new Color("#FFCB8E").convertSRGBToLinear(),
+            side: DoubleSide,
+            transparent: true,
+            opacity: 0.5,
+        })
+    )
+    ringScene.add(ring2);
+
+    const ring3 = new Mesh(
+        new RingGeometry(18, 17.75, 80),
+        new MeshPhysicalMaterial({
+            color: new Color("#FFCB8E").convertSRGBToLinear().multiplyScalar(50),
+            side: DoubleSide,
+            transparent: true,
+            opacity: 0.5,
+        })
+    )
+    ringScene.add(ring3);
+
 
     //adding the earth textures
     let textures = {
@@ -119,15 +164,20 @@ scene.add(sunLight);
 
         controls.update();
         renderer.render(scene, camera);
+
+        renderer.autoClear = false;
+        renderer.render(ringScene, ringCamera);
+        renderer.autoClear = true;
     });
 })();
 
 //function to create plane and add properties to it
-function makePlane(planeMesh, trail, envMap, scene) {
+function makePlane(planeMesh, trailT, envMap, scene) {
     let plane = planeMesh.clone();
     plane.scale.set(0.001,0.001,0.001);
     plane.position.set(0,0,0);
     plane.rotation.set(0,0,0);
+    plane.updateMatrixWorld();
 
     plane.traverse((object) => {
         if(object instanceof Mesh) {
@@ -137,8 +187,26 @@ function makePlane(planeMesh, trail, envMap, scene) {
         }
     });
 
+    let trail = new Mesh(
+        new PlaneGeometry(1, 2),
+        new MeshPhysicalMaterial({
+            envMap,
+            envmapIntensity: 3,
+            color: new Color(1, 0.5, 1),
+            roughness: 0.4,
+            metalness: 0,
+            transmission: 1,
+            transparent: true,
+            opacity: 1,
+            alphaMap: trailT,
+        })
+    )
+    trail.rotateX(Math.PI);
+    trail.translateY(1.1);
+
     let group = new Group();
     group.add(plane);
+    group.add(trail);
 
     scene.add(group);
 
